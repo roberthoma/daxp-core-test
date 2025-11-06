c375691341f8e71621b3191f9b5c74549c25fa3a
Robert Homa <roberthoma@yahoo.com>
2025-11-06 11:01:05 +0100

    Multi Customer message 2

--- src/main/java/com/daxprotocol/daxp_core_test/database/DataLoader.java
+++ src/main/java/com/daxprotocol/daxp_core_test/database/DataLoader.java
@@ -26,13 +26,9 @@ public class DataLoader implements CommandLineRunner {
         customerRepo.save(Customer.builder().name("Anna").email("anna@wp.pl").telephone("+48 123 456 789").build());
         customerRepo.save(Customer.builder().name("Robert").surname("Homa").email("robert.homa@daxprotocol.org").build());
 
-        customerRepo.findAll().forEach(c ->
-                System.out.println(c.getCustomerId() + " -> " + c.getName())
-        );
-
         contractRepository.save(Contract.builder().contract_no("1234567876").amount(2000L).customerId(1L).build());
         contractRepository.save(Contract.builder().contract_no("2345678767").amount(4000L).customerId(1L).build());
-        contractRepository.save(Contract.builder().contract_no("2345678767").amount(3000L).customerId(3L).build());
+        contractRepository.save(Contract.builder().contract_no("3456787678").amount(3000L).customerId(3L).build());
     }
 }
 
